b49a468b4c62a48027222a7476127d16fc2cada6
geekane <32669380+geekane@users.noreply.github.com>
2025-09-10 15:15:08 +0800

    Update api.ts
    增加了对4.0模型的支持

--- src/api.ts
+++ src/api.ts
@@ -9,6 +9,7 @@ import crc32 from 'crc32';
 
 // 模型映射
 const MODEL_MAP: Record<string, string> = {
+  'jimeng-4.0': 'high_aes_general_v40',
   'jimeng-3.0': 'high_aes_general_v30l:general_v3.0_18b',
   'jimeng-2.1': 'high_aes_general_v21_L:general_v2.1_L',
   'jimeng-2.0-pro': 'high_aes_general_v20_L:general_v2.0_L',
